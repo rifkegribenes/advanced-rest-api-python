664410cd83c9823fcf40f71304df2e062c333cd9
rifkegribenes <rifkegribenes@gmail.com>
2019-02-15 22:21:29 -0800

    improve errors with constants

--- code/resources/store.py
+++ code/resources/store.py
@@ -1,18 +1,23 @@
 from flask_restful import Resource
 from models.store import StoreModel
 
+STORE_NOT_FOUND = "Store not found."
+STORE_DELETED = "Store deleted."
+NAME_ALREADY_EXISTS = "A store with name '{}' already exists."
+INSERT_ERROR = "An error occurred while creating the store."
+
 
 class Store(Resource):
     def get(self, name: str):
         store = StoreModel.find_by_name(name)
         if store:
             return store.json()
-        return {"message": "Store not found."}, 404
+        return {"message": STORE_NOT_FOUND}, 404
 
     def post(self, name: str):
         if StoreModel.find_by_name(name):
             return (
-                {"message": "A store with name '{}' already exists.".format(name)},
+                {"message": NAME_ALREADY_EXISTS.format(name)},
                 400,
             )
 
@@ -20,7 +25,7 @@ class Store(Resource):
         try:
             store.save_to_db()
         except:
-            return {"message": "An error occurred while creating the store."}, 500
+            return {"message": INSERT_ERROR}, 500
 
         return store.json(), 201
 
@@ -29,7 +34,7 @@ class Store(Resource):
         if store:
             store.delete_from_db()
 
-        return {"message": "Store deleted."}
+        return {"message": STORE_DELETED}
 
 
 class StoreList(Resource):
